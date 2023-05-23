bc9bd22839c58e222d3502d3244ba871a8515fc4
Robert Eikmanns <r.eikmanns@alexander-buerkle.de>
2023-05-23 22:15:22 +0200

    add main shell loop, implement shelly read line

---
+++ shell.c
@@ -0,0 +1,48 @@
+#include "shelly.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // Load config files. if any. 
+
+    // Run command loop
+    shelly_loop();
+
+    // perform any shutdown/cleanup
+    return 0; 
+}
+
+void shelly_loop(void)
+{
+    char *line;
+    char **args;
+    int status = 1;
+
+    do {
+        printf("$ ");
+        line = shelly_read_line();
+        //args = shelly_split_line(line);
+        //status = shelly_execute(args);
+        printf("top line: %s\n", line);
+    } while (status);
+}
+
+char *shelly_read_line(void)
+{
+    int bufsize = SHELLY_BUF;
+    char *buffer = (char *) malloc(sizeof(char) * bufsize);
+    char *buf = buffer;
+    int c; 
+
+    while ((c = getchar()) != EOF && c != '\n') {
+        *buf++ = c;
+
+        if(buf - buffer == bufsize) {
+            
+        }
+    }
+
+    *buf = '\0';
+    return buffer;
+}
