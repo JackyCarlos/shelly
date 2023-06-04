690ae71959aa24f87527c58ccea3aa128818141e
Robert Eikmanns <r.eikmanns@alexander-buerkle.de>
2023-06-04 21:15:19 +0200

    add execute and num_builtins function prtotypes

--- shelly.h
+++ shelly.h
@@ -6,7 +6,12 @@ void shelly_loop(void);
 char *shelly_read_line(void);
 char **shelly_split_line(char *);
 int shelly_launch(char **);
+int shelly_execute(char **);
 
+// prototypes for shelly builtins
 int shelly_cd(char **args);
 int shelly_help(char **args);
 int shelly_exit(char **args);
+
+// prototypes for shelly helper function
+int shelly_num_builtins(void);
