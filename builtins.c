863cbe09d6d4a380ca911a31279a3258be9d3d3d
Robert Eikmanns <r.eikmanns@alexander-buerkle.de>
2023-06-04 21:15:51 +0200

    add source file for builtins

--- builtins.c
+++ builtins.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <unistd.h>
 #include "shelly.h"
 
 char *builtin_str[] = {
@@ -5,3 +7,43 @@ char *builtin_str[] = {
     "help",
     "exit"
 };
+
+int (*builtin[]) (char ** ) = {
+    &shelly_cd,
+    &shelly_help,
+    &shelly_exit
+};
+
+int shelly_num_builtins(void) {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/* Builtin function implementations */
+int shelly_cd(char **args)
+{
+    if (args[1] == NULL)
+        fprintf(stderr, "shelly: expected argument to \"cd\"\n");
+    else 
+        if (chdir(args[1]) != 0)
+            perror("shelly");
+    
+    return 0;
+}
+
+int shelly_help(char **args)
+{
+    int i;
+    printf("shelly by Robert Eikmanns\n");
+    printf("The following builtins are supported: \n");
+
+    for (i = 0; i < shelly_num_builtins(); i++)
+        printf("   %s\n", builtin_str[i]);
+    
+    printf("use the man command for Information on other programs");
+    return 1; 
+}
+
+int shelly_exit(char **args)
+{
+    return 0;
+}
