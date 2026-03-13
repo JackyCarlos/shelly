63210af392ba63261fadf479f50a2fccf0a8a6ce
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2026-03-13 16:01:08 +0100

    allocate more memory for context and token arrays if necessary, don't skip word token when in context pipe status

--- shelly.c
+++ shelly.c
@@ -95,27 +95,41 @@ int launch_command(execution_context_t *context) {
     return 1;
 }
 
+static void initiate_contexts(execution_context_t *contexts) {
+    int i;
+
+    for (i = 0; i < 32; ++i) {
+        contexts[i].tokens_index = 0;
+    }
+}
+
 execution_context_t *get_context(token_t *token_list) {
     execution_context_t *contexts;
     int i;
     context_status status;
+
+    int context_array_size;
+
     int tokens_index;   // index in the token array of a single context 
     int tokens_size;    // size of the char * array of a single context
 
-    // replace 32 with symbolic constant 
-    // put in separate function to initiate contexts
-    contexts = (execution_context_t *) malloc(sizeof(execution_context_t) * 32);
-    for (int j; j < 32; ++j) {
-        contexts[j].tokens_index = 0;
-    }
+    context_array_size = 32;
+    contexts = (execution_context_t *) malloc(sizeof(execution_context_t) * context_array_size);    // CONTEXT_ARRAY_SIZE
+    initiate_contexts(contexts);
     
     i = 0;
 
-    contexts[0].type = CONTEXT_END_TYPE;
+    contexts[i].type = CONTEXT_END_TYPE;
     status = STATUS_CONTEXT_INIT;
 
     while (token_list->type != NULL_TYPE) {
-                switch (token_list->type) {
+        if (i == context_array_size - 1) {
+            context_array_size += 32;
+            contexts = realloc(contexts, sizeof(execution_context_t) * context_array_size);
+            initiate_contexts(contexts + i + 1);
+        }
+
+        switch (token_list->type) {
             case REDIRECT_OUT_TYPE:
                 if (status != STATUS_CONTEXT_WORD) {
                     return NULL;
@@ -136,8 +150,8 @@ execution_context_t *get_context(token_t *token_list) {
                 if (status != STATUS_CONTEXT_WORD) {
                     return NULL;
                 }
-                contexts[i].flags |= INTO_PIPE;
-                i++;
+
+                contexts[i++].flags |= INTO_PIPE;
                 status = STATUS_CONTEXT_PIPE;
                 break;
 
@@ -162,19 +176,26 @@ execution_context_t *get_context(token_t *token_list) {
                     contexts[i].append_file = token_list->str;
                     contexts[i].flags |= APPEND;
 
-                } else if (status == STATUS_CONTEXT_PIPE) {
-                    contexts[i].flags |= OUT_OF_PIPE;
-
                 } else {
+                    if (status == STATUS_CONTEXT_PIPE) {
+                        contexts[i].flags |= OUT_OF_PIPE;
+                    }
+
                     tokens_index = contexts[i].tokens_index;
 
                     if (tokens_index == 0) {
                         contexts[i].type = CONTEXT_COMMAND_TYPE;
                         contexts[i + 1].type = CONTEXT_END_TYPE;
 
-                        tokens_size = 8;
+                        tokens_size = 32;
                         contexts[i].tokens = malloc(sizeof(char *) * tokens_size);
-                    } // left to do check for tokens_index - 1 == tokens_size and reallocate if necessary
+
+                    } else if (tokens_index - 1 == tokens_size) {
+                        tokens_size += 32;
+                        contexts[i].tokens = realloc(contexts[i].tokens, tokens_size);
+                    } 
+                    
+                    // left to do check for tokens_index - 1 == tokens_size and reallocate if necessary
 
                     contexts[i].tokens[tokens_index++] = token_list->str;
                     contexts[i].tokens[tokens_index] = NULL;
