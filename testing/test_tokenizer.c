42bef54abfbdd397474c5f45f79eef52a2545290
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2026-04-18 23:42:19 +0200

    add tests for tokenizing ampersand &

--- testing/test_tokenizer.c
+++ testing/test_tokenizer.c
@@ -33,6 +33,7 @@ void test_tokenizer_word_tokens_only(void) {
         TEST_ASSERT_TRUE(token_list2->type == TOK_WORD_TYPE);  
         token_list2++;  
     }
+    TEST_ASSERT_TRUE(token_list2->type == TOK_NULL_TYPE); 
     
     TEST_ASSERT_TRUE(strcmp(token_list[0].str, "echo") == 0);
     TEST_ASSERT_TRUE(strcmp(token_list[1].str, "aaa") == 0);
@@ -40,6 +41,21 @@ void test_tokenizer_word_tokens_only(void) {
     TEST_ASSERT_TRUE(strcmp(token_list[3].str, "ccc") == 0);
     TEST_ASSERT_TRUE(strcmp(token_list[4].str, "444") == 0);
     TEST_ASSERT_TRUE(strcmp(token_list[5].str, "!!!") == 0);
+}
+
+void test_tokenizer_ampersand_only(void) {
+    token_t *token_list;
+ 
+    token_list = tokenizer("&");
+    TEST_ASSERT_TRUE(token_list[0].type == TOK_AMPS_TYPE);
+    TEST_ASSERT_TRUE(token_list[1].type == TOK_NULL_TYPE);
+
+    token_list = tokenizer("&&&&");
+    TEST_ASSERT_TRUE(token_list[0].type == TOK_AMPS_TYPE);
+    TEST_ASSERT_TRUE(token_list[1].type == TOK_AMPS_TYPE);
+    TEST_ASSERT_TRUE(token_list[2].type == TOK_AMPS_TYPE);
+    TEST_ASSERT_TRUE(token_list[3].type == TOK_AMPS_TYPE);
+    TEST_ASSERT_TRUE(token_list[4].type == TOK_NULL_TYPE);
 }
 
 void test_tokenizer_redirect_tokens_only(void) {
@@ -130,13 +146,12 @@ void test_tokenizer_long_word_tokens(void) {
     TEST_ASSERT_TRUE(strcmp(token_list[0].str, c33) == 0);
 }
 
-
-
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_tokenizer_ignore_blank_characters);
     RUN_TEST(test_tokenizer_word_tokens_only);
+    RUN_TEST(test_tokenizer_ampersand_only);
     RUN_TEST(test_tokenizer_redirect_tokens_only);
     RUN_TEST(test_tokenizer_mixed_tokens1); 
     RUN_TEST(test_tokenizer_mixed_tokens2); 
