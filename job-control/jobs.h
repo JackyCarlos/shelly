25b95d0b363910f1ed11abf2d039d2b9a73123ba
Robert Eikmanns <robert.eikmanns@stud.htwk-leipzig.de>
2026-05-03 10:28:00 +0200

    add structures for single jobs and their corresponding commands, add function prototype for initializing job control and adding job_commands to jobs

---
+++ job-control/jobs.h
@@ -0,0 +1,30 @@
+#include "../parser/parser.h"
+
+typedef struct job_command {
+    int status; // 
+
+    char **tokens;
+    int argc;
+
+    redirect_flags flags;
+    char *input_file;
+    char *output_file;
+    char *append_file;
+
+    int pid;
+    int return_value;
+
+} job_command_t;
+
+typedef struct job {
+    int id;
+    int job_cmd_counter;
+
+    job_command_t *job_commands;
+    int job_cmds_size;
+} job_t;
+
+
+
+void init_job_control(void);
+void add_background_job(execution_context_t *context);
