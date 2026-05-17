510f27ffb0ed05e26dac02fdb357d8658028dcac
Robert Eikmanns <robert.eikmanns@stud.htwk-leipzig.de>
2026-05-17 16:25:47 +0200

    add function prototypes for fg and bg builtins

--- builtins/builtins_registry.c
+++ builtins/builtins_registry.c
@@ -5,15 +5,19 @@
 int builtin_exit(char **tokens);
 int builtin_change_directory(char **tokens);
 int builtin_type(char **tokens);
-int job_control_builtin_jobs(char **tokens);
 
 /* job control builtins */
+int job_control_builtin_jobs(char **tokens);
+int job_control_builtin_fg(char **tokens);
+int job_control_builtin_bg(char **tokens);
 
 const builtin_t builtins[] = {
     { "exit", builtin_exit },
     { "cd", builtin_change_directory},
     { "type", builtin_type }, 
-    { "jobs", job_control_builtin_jobs }
+    { "jobs", job_control_builtin_jobs }, 
+    { "fg", job_control_builtin_fg }, 
+    { "bg",  job_control_builtin_bg }
 };
 
 const int builtins_size = sizeof(builtins) / sizeof(builtin_t);
