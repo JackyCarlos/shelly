3240cddc3b903622d9d8c9e336107fa6204139a8
Robert Eikmanns <robert.eikmanns@stud.htwk-leipzig.de>
2026-05-26 19:44:27 +0200

    implement bg builtin, make get_jobid handle errors for both fg and bg builtins, add function job_mark_running which marks every suspended job command inside of a job as running

--- builtins/jobs_control_builtins.c
+++ builtins/jobs_control_builtins.c
@@ -7,7 +7,8 @@
 #include "../job-control/jobs.h"
 #include "../common/command_flags.h"
 
-static int get_jobid(char **);
+static int get_jobid(char **, char *);
+static void job_mark_running(job_t *job);
 static void print_job(job_t *job);
 static int print_command_group(job_t *job, int start_index);
 static void print_command(job_command_t *cmd);
@@ -21,7 +22,7 @@ int job_control_builtin_fg(char **tokens) {
     int job_id;
     job_t *job;
 
-    job_id = get_jobid(tokens);
+    job_id = get_jobid(tokens, "fg");
 
     if (job_id == -1) {
         return -1;
@@ -31,11 +32,7 @@ int job_control_builtin_fg(char **tokens) {
     job->is_background = 0;
 
     if (job->status == SUSPENDED) {
-        for (int i = 0; i < job->job_cmd_counter; ++i) {
-            if (job->job_commands[i].job_stat == CMD_SUSPENDED) {
-                job->job_commands[i].job_stat = CMD_RUNNING;
-            }
-        }
+        job_mark_running(job);
 
         if (kill(-job->pgid, SIGCONT) == -1) {
             fprintf(stderr, "error continuing job %d\n", job_id);
@@ -45,18 +42,46 @@ int job_control_builtin_fg(char **tokens) {
     return job_control_after_launch(job);
 }
 
-static int get_jobid(char **tokens) {
+int job_control_builtin_bg(char **tokens) {
+    int job_id;
+    job_t *job;
+
+    job_id = get_jobid(tokens, "bg");
+
+    if (job_id == -1) {
+        return -1;
+    }
+
+    job = &job_list[job_id];
+
+    if (job->status == RUNNING && job->is_background == 1) {
+        fprintf(stderr, "bg: job already in background\n");
+        return -1;
+    }
+
+    job_mark_running(job);
+
+    if (kill(-job->pgid, SIGCONT) == -1) {
+        fprintf(stderr, "error continuing job %d\n", job_id);
+    }
+
+    job->status = RUNNING;
+
+    return 0;
+}
+
+static int get_jobid(char **tokens, char *job_builtin) {
     int job_id;
     char *end_ptr;
     
     if (tokens[1] == NULL || tokens[2] != NULL || *tokens[1] != '%') {
-        printf("fg: usage: fg %%<id>\n");
+        printf("%s: usage: %s %%<id>\n", job_builtin, job_builtin);
         return -1;
     }
 
     job_id = strtol(tokens[1] + 1, &end_ptr, 10);
     if (*end_ptr != '\0') {
-        printf("fg: usage: fg %%<id>\n");
+        printf("%s: usage: %s %%<id>\n", job_builtin, job_builtin);
         return -1;
     }
 
@@ -65,16 +90,21 @@ static int get_jobid(char **tokens) {
     if (job_id >= job_list_size || job_id < 0 ||
             job_list[job_id].id == -1 || !job_list[job_id].is_background) {
 
-        printf("fg: %%%d: no such job\n", job_id + 1);
+        printf("%s: %%%d: no such job\n", job_builtin, job_id + 1);
         return -1;
     }
 
     return job_id;
 }
 
-int job_control_builtin_bg(char **tokens) {
-    return 0;
+static void job_mark_running(job_t *job) {
+    for (int i = 0; i < job->job_cmd_counter; ++i) {
+        if (job->job_commands[i].job_stat == CMD_SUSPENDED) {
+            job->job_commands[i].job_stat = CMD_RUNNING;
+        }
+    }    
 }
+
 
 int job_control_builtin_jobs(char **tokens) {
     int job_id;
